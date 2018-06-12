cd2525f74001ea0ec6912958126aa242ee2e8cc1
klx3300 <inc19991@gmail.com>
2018-06-12 11:35:17 +0800

    seems almost done.

--- src/main/main.go
+++ src/main/main.go
@@ -2,11 +2,42 @@ package main
 
 import (
 	"configrd"
+	"logger"
+	"net/http"
 	"os"
+	"push"
+	"status"
 )
+
+var notif = push.NewNotificationMgr()
+var servstat = status.NewServiceStatus()
 
 func main() {
 	var confile = configrd.Config(os.Args[1])
 	conf := confile.ReadConfig()
+	_, mpok := conf["NotificationSave"]
+	if !mpok {
+		panic("NotificationSave has to be exist in config map!")
+	}
+	notif.Load(conf["NotificationSave"])
+	_, mpok = conf["ServiceSave"]
+	if !mpok {
+		panic("ServiceSave has to be exist in config map!")
+	}
+	servstat.Load(conf["ServiceSave"])
+	_, mpok = conf["ListenPort"]
+	if !mpok {
+		panic("ListenPort has to be exist in config map!")
+	}
+	http.HandleFunc("/fetch", onFetch)
+	http.HandleFunc("/callback", onCallback)
+	logger.Log.Logln(logger.LEVEL_WARNING, "Listen", http.ListenAndServe(":"+conf["ListenPort"], nil))
+}
+
+func onFetch(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func onCallback(w http.ResponseWriter, r *http.Request) {
 
 }
